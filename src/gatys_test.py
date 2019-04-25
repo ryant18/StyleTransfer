b8a4ba7e564e9ba5e8743a6ef86ac934c4d32c45
ryan <ryant18@vt.edu>
2019-04-25 18:46:43 -0400

    change path to os join

--- src/gatys_test.py
+++ src/gatys_test.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import image_util as img
 import model
 import matplotlib as mpl
+import os
 mpl.rcParams['figure.figsize'] = (10,10)
 mpl.rcParams['axes.grid'] = False
 
@@ -11,18 +12,8 @@ tf.enable_eager_execution()
 print("Eager execution: {}".format(tf.executing_eagerly()))
 
 # Set up some global values here
-content_path = '../images/Green_Sea_Turtle_grazing_seagrass.jpg'
-style_path =   '../images/The_Great_Wave_off_Kanagawa.jpg'
-
-content = img.load(content_path).astype('uint8')
-style = img.load(style_path)
-
-plt.subplot(1, 2, 1)
-img.show(content, 'Content Image')
-
-plt.subplot(1, 2, 2)
-img.show(style, 'Style Image')
-plt.show()
+content_path = os.path.join('..',  'images', 'Green_Sea_Turtle_grazing_seagrass.jpg')
+style_path = os.path.join('..', 'images', 'The_Great_Wave_off_Kanagawa.jpg')
 
 best, best_loss = model.run_style_transfer(content_path, style_path, num_iterations=1000)
 
