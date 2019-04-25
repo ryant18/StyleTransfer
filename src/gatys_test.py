c85fe5744de3a4e3b0a53a16550a09238f432a79
Ryan <ryant18@vt.edu>
2019-04-25 14:43:52 -0400

    Gatys model test

---
+++ src/gatys_test.py
@@ -0,0 +1,45 @@
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import image_util as img
+import model
+import matplotlib as mpl
+mpl.rcParams['figure.figsize'] = (10,10)
+mpl.rcParams['axes.grid'] = False
+
+
+tf.enable_eager_execution()
+print("Eager execution: {}".format(tf.executing_eagerly()))
+
+# Set up some global values here
+content_path = '../images/Green_Sea_Turtle_grazing_seagrass.jpg'
+style_path =   '../images/The_Great_Wave_off_Kanagawa.jpg'
+
+img.show(content_path, "Content")
+img.show(style_path, "Style")
+
+best, best_loss = model.run_style_transfer(content_path, style_path, num_iterations=1000)
+
+
+def show_results(best_img, content_path, style_path, show_large_final=True):
+    plt.figure(figsize=(10, 5))
+    content = img.load(content_path)
+    style = img.load(style_path)
+
+    plt.subplot(1, 2, 1)
+    img.show(content, 'Content Image')
+
+    plt.subplot(1, 2, 2)
+    img.show(style, 'Style Image')
+
+    if show_large_final:
+        plt.figure(figsize=(10, 10))
+
+        plt.imshow(best_img)
+        plt.title('Output Image')
+        plt.show()
+
+
+show_results(best, content_path, style_path)
+
+
+
---
+++ src/image_util.py
@@ -0,0 +1,57 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
+from tensorflow.python.keras.preprocessing import image as kp_image
+import tensorflow as tf
+
+
+def load(path_to_img):
+    max_dim = 512
+    img = Image.open(path_to_img)
+    long = max(img.size)
+    scale = max_dim/long
+    img = img.resize((round(img.size[0]*scale), round(img.size[1]*scale)), Image.ANTIALIAS)
+
+    img = kp_image.img_to_array(img)
+
+    # We need to broadcast the image array such that it has a batch dimension
+    img = np.expand_dims(img, axis=0)
+    return img
+
+
+def show(img, title=None):
+    loaded_img = load(img)
+    # Remove the batch dimension
+    out = np.squeeze(loaded_img, axis=0)
+    # Normalize for display
+    out = out.astype('uint8')
+    plt.imshow(out)
+    if title is not None:
+        plt.title(title)
+    plt.imshow(out)
+    plt.show()
+
+
+def load_and_process(path_to_img):
+    img = load(path_to_img)
+    img = tf.keras.applications.vgg19.preprocess_input(img)
+    return img
+
+
+def deprocess(processed_img):
+    x = processed_img.copy()
+    if len(x.shape) == 4:
+        x = np.squeeze(x, 0)
+    assert len(x.shape) == 3, ("Input to deprocess image must be an image of "
+                               "dimension [1, height, width, channel] or [height, width, channel]")
+    if len(x.shape) != 3:
+        raise ValueError("Invalid input to deprocessing image")
+
+    # perform the inverse of the preprocessiing step
+    x[:, :, 0] += 103.939
+    x[:, :, 1] += 116.779
+    x[:, :, 2] += 123.68
+    x = x[:, :, ::-1]
+
+    x = np.clip(x, 0, 255).astype('uint8')
+    return x
