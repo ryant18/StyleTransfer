e052af98c5d210a7d630e83fce7e3ed28753647e
ryan <ryant18@vt.edu>
2019-04-26 08:47:57 -0400

    not working nameing output

--- src/gatys_test.py
+++ src/gatys_test.py
@@ -7,63 +7,65 @@ import os
 mpl.rcParams['figure.figsize'] = (10, 10)
 mpl.rcParams['axes.grid'] = False
 
-# Set Images --------------------------------------------
-content_image_name = 'cardinal'
-style_image_name = 'wave'
 
-# Load Images -------------------------------------------
-tf.enable_eager_execution()
-print("Eager execution: {}".format(tf.executing_eagerly()))
-content_path = os.path.join('..',  'images', content_image_name+'.jpg')
-style_path = os.path.join('..', 'images', style_image_name+'.jpg')
+def create_image(content_path, style_path, show_output=False, save_output=True):
+    tf.enable_eager_execution()
+    print("Eager execution: {}".format(tf.executing_eagerly()))
+    # Run model ---------------------------------------------
+    best_img, best_loss, timeline_imgs = model.run_style_transfer(content_path, style_path, num_iterations=1000)
 
-# Run model ---------------------------------------------
-best_img, best_loss, timeline_imgs = model.run_style_transfer(content_path, style_path, num_iterations=1000)
+    # Plot outputs ------------------------------------------
+    content = img.load(content_path)
+    style = img.load(style_path)
 
-# Plot outputs ------------------------------------------
-content = img.load(content_path)
-style = img.load(style_path)
-
-plt.figure(figsize=(15, 5))
-plt.subplot(1, 3, 1)
-img.show(content)
-plt.xticks([])
-plt.yticks([])
-plt.subplot(1, 3, 2)
-img.show(style)
-plt.xticks([])
-plt.yticks([])
-plt.subplot(1, 3, 3)
-plt.imshow(best_img)
-plt.xticks([])
-plt.yticks([])
-fig_compare = plt.gcf()
-plt.show()
-
-plt.figure(figsize=(10, 10))
-plt.imshow(best_img)
-plt.xticks([])
-plt.yticks([])
-fig_output = plt.gcf()
-plt.show()
-
-num_rows = 2
-num_cols = 5
-plt.figure(figsize=(14, 4))
-for i, img in enumerate(timeline_imgs):
-    plt.subplot(num_rows, num_cols, i+1)
-    plt.imshow(img)
+    plt.figure(figsize=(15, 5))
+    plt.subplot(1, 3, 1)
+    img.show(content)
     plt.xticks([])
     plt.yticks([])
-fig_timeline = plt.gcf()
-plt.show()
+    plt.subplot(1, 3, 2)
+    img.show(style)
+    plt.xticks([])
+    plt.yticks([])
+    plt.subplot(1, 3, 3)
+    plt.imshow(best_img)
+    plt.xticks([])
+    plt.yticks([])
+    fig_compare = plt.gcf()
+    if show_output:
+        plt.show()
 
-# Save Images ---------------------------------------------
-output_name = content_image_name + ' '+style_image_name+'.jpg'
-fig_output.savefig(os.path.join('..', 'output', output_name), bbox_inches='tight')
-fig_compare.savefig(os.path.join('..', 'compare', output_name), bbox_inches='tight')
-fig_timeline.savefig(os.path.join('..', 'timeline', output_name), bbox_inches='tight')
+    plt.figure(figsize=(10, 10))
+    plt.imshow(best_img)
+    plt.xticks([])
+    plt.yticks([])
+    fig_output = plt.gcf()
+    if show_output:
+        plt.show()
+
+    num_rows = 2
+    num_cols = 5
+    plt.figure(figsize=(14, 4))
+    for i, image in enumerate(timeline_imgs):
+        plt.subplot(num_rows, num_cols, i+1)
+        plt.imshow(image)
+        plt.xticks([])
+        plt.yticks([])
+    fig_timeline = plt.gcf()
+    if show_output:
+        plt.show()
+
+    # Save Images ---------------------------------------------
+    output_name = os.path.basename(content_path.split('.')[0]) + ' ' + \
+                  os.path.basename(style_path.split('.')[0]) + '.jpg'
+
+    if save_output:
+        fig_output.savefig(os.path.join('..', 'output', output_name), bbox_inches='tight')
+        fig_compare.savefig(os.path.join('..', 'compare', output_name), bbox_inches='tight')
+        fig_timeline.savefig(os.path.join('..', 'timeline', output_name), bbox_inches='tight')
 
 
-
+content_path = os.path.join('..',  'images', 'cardinal' + '.jpg')
+style_path = os.path.join('..', 'images', 'starry night' + '.jpg')
+create_image(content_path, style_path)
 
