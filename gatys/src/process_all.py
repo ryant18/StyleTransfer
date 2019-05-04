000d33e334d96aa688bf8f319369a0d14dcdd21e
ryan <ryant18@vt.edu>
2019-05-04 10:49:25 -0400

    moved gates to own folder

--- gatys/src/process_all.py
+++ gatys/src/process_all.py
@@ -2,11 +2,11 @@ import os
 import gatys_test
 
 # Run all combinations of images
-images = [os.path.join('..', 'images', name) for name in os.listdir(os.path.join('..', 'images'))]
+images = [os.path.join('..', '..', 'images', name) for name in os.listdir(os.path.join('..', 'images'))]
 for content in images:
     for style in images:
         if content is not style:
             output_name = os.path.basename(content).split('.')[0] + ' ' + \
                           os.path.basename(style).split('.')[0] + '.jpg'
-            if not os.path.isfile(os.path.join('..', 'output', output_name)):
+            if not os.path.isfile(os.path.join('..', '..', 'output', output_name)):
                 gatys_test.create_image(content, style, show_output=True)
