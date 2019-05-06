963391b6506aba76b916019e879443d42578499d
ryan <ryant18@vt.edu>
2019-05-06 19:51:33 -0400

    fixed image path

--- gatys/src/process_all.py
+++ gatys/src/process_all.py
@@ -2,7 +2,7 @@ import os
 import gatys_test
 
 # Run all combinations of images
-images = [os.path.join('..', '..', 'images', name) for name in os.listdir(os.path.join('..', 'images'))]
+images = [os.path.join('..', '..', 'images', name) for name in os.listdir(os.path.join('..', '..', 'images'))]
 for content in images:
     for style in images:
         if content is not style:
