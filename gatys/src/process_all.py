19013c446ad10db2efb6a5c935642cb0250c03f2
ryan <ryant18@vt.edu>
2019-05-06 22:30:47 -0400

    all images done for gatys

--- gatys/src/process_all.py
+++ gatys/src/process_all.py
@@ -8,5 +8,5 @@ for content in images:
         if content is not style:
             output_name = os.path.basename(content).split('.')[0] + ' ' + \
                           os.path.basename(style).split('.')[0] + '.jpg'
-            if not os.path.isfile(os.path.join('..', '..', 'output', output_name)):
+            if not os.path.isfile(os.path.join('..', 'output', output_name)):
                 gatys_test.create_image(content, style, show_output=True)
